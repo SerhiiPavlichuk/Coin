36cfee11dea1989f294140591bbee0fc81d816e1
SerhiiPavlichuk <S_Pavlichuk@icloud.com>
2022-04-18 14:31:57 +0300

    coinManagerDelegate

--- Coin/Model/CoinManager.swift
+++ Coin/Model/CoinManager.swift
@@ -7,12 +7,42 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdatePrice(_ coinManager: CoinManager, coinPrice: CoinModel)
+    func didFailWithError(error:Error)
+}
+
 struct CoinManager {
 
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let baseURL: String
+    let apiKey: String
+    var delegate: CoinManagerDelegate?
 
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
+    init() {
+        self.apiKey = (Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String)!
+        self.baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    }
+    
+    func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        performRequest(with: urlString)
+    }
+
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let saveData = data {
+                    
+                }
+            }
+        }
+    }
 
 }
---
+++ Coin/Model/CoinModel.swift
@@ -0,0 +1,12 @@
+//
+//  CoinModel.swift
+//  Coin
+//
+//  Created by admin on 18.04.2022.
+//
+
+import Foundation
+
+struct CoinModel {
+    
+}
